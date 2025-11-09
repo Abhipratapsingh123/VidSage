f4f89bfa7d6f53bbf9b1c4b403624753135827c0
Abhipratapsingh123 <fee.mca941@gmail.com>
2025-11-09 12:19:59 +0530

    depriciated-libraries-updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
